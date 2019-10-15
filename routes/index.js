f726de200d9e569933e52c6b1a2ef001ab8ce4a0
loisguillet <lois.guillet@gmail.com>
2019-10-15 21:06:23 +0200

    test with redis

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
 })
 
 var redis = require('redis');
-var client = redis.createClient(6379,"localhost");
+var client = redis.createClient(6379,"localhoct");
 
 client.on('connect', function() {
   console.log('Redis client connected');
