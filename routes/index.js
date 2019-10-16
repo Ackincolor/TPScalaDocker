02ed96cc6c684a4e13930987a40dca6464494c5a
loisguillet <lois.guillet@gmail.com>
2019-10-16 09:08:42 +0200

    test with redis

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var redis = require('redis');
+var client = redis.createClient(6379,"localhost");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  client.get('my test key', function (error, result) {
+  client.set('1', 'my test value', redis.print);
+  client.get('1', function (error, result) {
     if (error) {
       console.log(error);
       throw error;
@@ -14,9 +16,6 @@ router.get('/', function(req, res, next) {
 
 })
 
-var redis = require('redis');
-var client = redis.createClient(6379,"192.168.0.187");
-
 client.on('connect', function() {
   console.log('Redis client connected');
 });
@@ -25,7 +24,7 @@ client.on('error', function (err) {
   console.log('Something went wrong ' + err);
 });
 
-client.set('my test key', 'my test value', redis.print);
+
 
 
 module.exports = router;
